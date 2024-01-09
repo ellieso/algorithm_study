85395bcf72155a77d266363e765759fa32184d67
ellieso <83899219+ellieso@users.noreply.github.com>
2024-01-10 02:43:37 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 2.88 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,36 +1,33 @@
 def solution(bandage, health, attacks):
-
-    bandage_time = 0
-    additional_health = 0
-    final_health = health
-
-    while len(attacks) > 0:
-        attack = attacks[0]
-
-        # 캐릭터의 체력이 0 이하가 되면 -1을 리턴
-        if final_health <= 0:
-            final_health = -1
-            break
-
-        # 공격시간과 경과 시간이 같으면 health에서 피해량 빼기 / 공격성공이므로 연속성공 초기화
-        if attack[0] == bandage_time:
-            final_health -= attack[1]
-            additional_health = 0
-            attacks.pop(0)
-        # 공격시간과 경과 시간이 다르면 회복
-        else:
-            # 최대 체력과 동일하지 않으면 연속공격이 성공했을 때만 추가회복
-            if final_health < health:
-                additional_health += 1
-
-            # 연속 공격에 성공하면 추가회복
-            if additional_health == bandage[0]:
-                final_health = min(final_health + bandage[1] + bandage[2], health)
-                additional_health = 0
-            # 연속 공격에 성공하지 않으면 기본 값만 회복
-            else:
-                final_health = min(final_health + bandage[1], health)
-
-        bandage_time += 1
+    final_attack = attacks[-1][0]
     
-    return final_health if final_health > 0 else -1
+    for i in range(1, final_attack+1):
+        if attacks[i-1][0] != i:
+            attacks.insert(i-1, [i,0])
+    print(f"new_attack : {attacks}")
+    
+    answer = health
+    heal_time = 0
+    
+    for i in range(final_attack):
+        print(f"{i+1}초")
+        if attacks[i][1] != 0:
+            answer = answer - attacks[i][1]
+            heal_time = 0
+            print(f"공격받았습니다. 현재 체력 : {answer}, 힐 연속시간 : {heal_time}초")
+            if answer <= 0:
+                # 체력이 0 이하이면 캐릭터가 사망
+                answer = -1
+                print("캐릭터가 사망하였습니다.")
+                return answer
+        else:
+            heal_time += 1
+            
+            if heal_time != bandage[0]:
+                answer = min(answer + bandage[1], health)
+                print(f"공격받지 않아 붕대감기를 시작합니다. 현재 체력 : {answer}, 힐 연속시간 : {heal_time}초")
+            else:
+                answer = min(answer + bandage[1] + bandage[2], health)
+                heal_time = 0
+                print(f"힐 지속시간 최대치로 추가 체력을 회복합니다. 현재 체력 : {answer}, 힐 연속시간 : {heal_time}초")
+    return answer
